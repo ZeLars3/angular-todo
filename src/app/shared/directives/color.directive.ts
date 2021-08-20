d86941cddb090d2eb1d0f4af480944def0b3d9e2
ZeLars3 <zelt378@gmail.com>
2021-08-20 17:34:22 +0300

    implemented color change by category

--- src/app/shared/directives/color.directive.ts
+++ src/app/shared/directives/color.directive.ts
@@ -11,12 +11,31 @@ import {
 })
 export class ColorDirective {
   @Input("color") color: string;
+  @Input("category") category: string;
+
   constructor(private element: ElementRef, private render: Renderer2) {
     this.render.setStyle(this.element.nativeElement, "color", this.color);
     element.nativeElement.style.color = this.color;
   }
 
   @HostListener("mouseenter") onMouseEnter() {
+    switch (this.category) {
+      case "General":
+        this.color = "blue";
+        break;
+      case "Work":
+        this.color = "red";
+        break;
+      case "Inbox":
+        this.color = "green";
+        break;
+      case "Personal":
+        this.color = "orange";
+        break;
+      case "Social":
+        this.color = "gold";
+        break;
+    }
     this.ChangeColor(this.color);
   }
 
